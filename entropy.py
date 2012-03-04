643585c3e6028dde3a33e2987d24695fcb04de86
Jean Gobin <jeanfgobin@gmail.com>
2012-03-04 11:49:04 -0500

    Added comments to entropy.py

--- entropy.py
+++ entropy.py
@@ -22,12 +22,16 @@ def Usage(execname):
 	return
 
 # Returns pr.log(pr) if pr>0, 0 otherwise
-# where 0<=pr<1
+# where 0<=pr<1. There is no explicit test to check 
+# that pr <= 1, but a negative number will just return 0
 
 def prlogpr(pr):
 	if pr > 0:	
 		return pr*math.log(pr)
 	return 0
+
+
+# Compute the entropy of a given file
 
 def ComputeFileEntropy(objname):
 	entropy=0
@@ -46,7 +50,7 @@ def ComputeFileEntropy(objname):
 			total=total+1.0
 		else:
 			break
-	# If we have anything, we'll compute the entropy.
+	# If we have anything, we compute the entropy.
 	# entropy=sum(i in I) -pr[i]*log(pr[i])
 	if total>0.0:
 		for i in range(256):
@@ -55,27 +59,35 @@ def ComputeFileEntropy(objname):
 		entropy=0
 	return entropy
 
+
 # Walkdir - if in dir mode, let's process each file
+# The order is to descend the dir tree first and then print the files.
+# The function is used recursively to walk down the tree
+
 def WalkDir(objname):
 	listofdir=[]
 	listoffiles=[]
 	listofentropy=[]
-	
+	# Get a list of the object in the current directory
 	listofitem=os.listdir(objname)
 	for item in listofitem:
+		# Add the previous path to get a path relative to where the script was started
 		absitem=os.path.join(objname,item)
+		# If it is a file, let's add it to the list of files
 		if os.path.isfile(absitem):
 			listoffiles.append(absitem)
 		else:
+		# Otherwise, let's add this as a directory
 			listofdir.append(absitem)
+	# And let's go down in each subdir
 	for item in listofdir:
 		WalkDir(item)
+	# If no more subdirectory, let's compute and print the entropy of each file
 	for item in listoffiles:
 		print item," : ",ComputeFileEntropy(item)
 	return
 
 # Main 
-
 
 def main(argv):
 	if len(argv) < 2:
